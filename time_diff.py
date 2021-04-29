d6bf669bd3110654b2df2309ab93147793c9b06c
Aakash Das <76225630+kaaash-aakash@users.noreply.github.com>
2021-04-29 12:23:28 +0530

    corrected

--- time_diff.py
+++ time_diff.py
@@ -4,7 +4,7 @@ duration = clip.duration  # in secs
 real_fps = (clip.fps)/1.001
 tot_frames = fps * duration
 def fram_diff(frame1, frame2):
-    return((frame1/real_fps),(frame2/real_fps))
+    return((frame1/real_fps)-(frame2/real_fps))
 
 
 
