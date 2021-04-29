094b6168eac138cbc2d16f4157791e33e071f358
Aakash Das <76225630+kaaash-aakash@users.noreply.github.com>
2021-04-29 12:21:57 +0530

    Add files via upload

---
+++ time_diff.py
@@ -0,0 +1,10 @@
+import moviepy.editor import VideoFileClip
+clip = VideoFileClip("fs3_60.mp4")
+duration = clip.duration  # in secs
+real_fps = (clip.fps)/1.001
+tot_frames = fps * duration
+def fram_diff(frame1, frame2):
+    return((frame1/real_fps),(frame2/real_fps))
+
+
+
