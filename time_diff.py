1631c66a4252358cbac338c655bf8bd43a2f955d
Aakash Das <76225630+kaaash-aakash@users.noreply.github.com>
2021-04-29 15:55:39 +0530

    final time difference

--- time_diff.py
+++ time_diff.py
@@ -1,14 +1,8 @@
 from moviepy.editor import VideoFileClip
-clip = VideoFileClip(r"res/Fan speed-3 1080p 60fps.mp4")
-duration = clip.duration  # in secs
-print(f"duration: {duration}")
-real_fps = (clip.fps)/1.001
-print(f"clip.fps: {clip.fps}")
-print(f"real_fps: {real_fps}")
-tot_frames = real_fps * duration
-print(f"tot_frame: {tot_frames}")
+
+
 def fram_diff(frame1, frame2):
+
+    clip = VideoFileClip(r"res/Fan speed-3 1080p 60fps.mp4")
+    real_fps = (clip.fps)/1.001
     return((frame1/real_fps)-(frame2/real_fps))
-
-
-
